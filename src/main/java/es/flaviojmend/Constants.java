34346b4e9de80253a5b20eb4033818f356c24dac
Flavio Mendes <flavio.mendes@eldorado.org.br>
2016-07-11 14:59:16 -0300

    commiting for future reference

---
+++ src/main/java/es/flaviojmend/Constants.java
@@ -0,0 +1,15 @@
+package es.flaviojmend;
+
+/**
+ * Created by flavio on 11/07/16.
+ */
+public class Constants {
+
+    /**
+     * properties file
+     */
+    public final static String Properties = "classpath:application.properties";
+
+    public final static String PropertyRealtimeServerAddress = "${realtime.server.address}";
+
+}
